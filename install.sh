752049e36440293fc418fe82aa9c6761c1692b34
Christof Marti <chrmarti@microsoft.com>
2020-09-10 10:39:43 +0200

    Remove dependency

--- install.sh
+++ install.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
 
-stow links
+FOLDER=$(cd `dirname $0` && pwd)
+
+for f in `ls -a $FOLDER/links`
+do
+	if [ ! -e $FOLDER/../$f ]
+	then
+		echo Linking $f
+		ln -s `basename $FOLDER`/links/$f `dirname $FOLDER`/$f
+	fi
+done
